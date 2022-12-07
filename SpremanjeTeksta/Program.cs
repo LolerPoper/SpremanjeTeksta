0a8887dc8e54316064998b0f9397703767e97492
LolerPoper <113007900+LolerPoper@users.noreply.github.com>
2022-12-07 09:22:02 +0100

    Add files via upload

---
+++ SpremanjeTeksta/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+namespace SpremanjeTeksta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Unesite ime: ");
+            string ime = Console.ReadLine();
+            Console.Write("Unesite prezime: ");
+            string prezime = Console.ReadLine();
+            StreamWriter sw = new StreamWriter(@"C:\Users\ucenik\Documents\LukaRadetic\datoteka.txt");
+            sw.WriteLine("Ime: {0}", ime);
+            sw.WriteLine("Prezime: {0}", prezime);
+            sw.Close();
+        }
+    }
+}
